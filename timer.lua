3cc1a050f13c2ce997634d2c9de6a342b5453423
dmitriy.pavlyuk <al.lastor@bk.ru>
2017-11-05 21:20:34 +0300

    Функции теперь определяются не через лямбды

--- timer.lua
+++ timer.lua
@@ -30,20 +30,22 @@ function File (string)
 end
 
 
-round = function(n, i)
+function round (n, i)
     if i then
         return round(n*i)/i
     end
     return n - n%1
 end
 
-addZeroToNumber = function (n)
+
+function addZeroToNumber (n)
     if #tostring(n) < 2 then return "0"..n end
     return ""..n
 end
 
+
 -- s => h:m:s
-toNormalTimeFormat = function (n)
+function toNormalTimeFormat (n)
     local s = round(n%60)
     local m = round(n/60%60)
     local h = round(n/3600%24)
@@ -56,7 +58,7 @@ if debug and "1:01:05" ~= toNormalTimeFormat(3665) then
 end
 
 
-printLine = function ()
+function printLine ()
     io.write[[
 -------------------------------------------------------------------------------
 ]]
@@ -77,6 +79,7 @@ SCTR - Simple Console Time Registrator
 ]]
     printLine()
 end
+
 
 function TableOfWorkTime ()
     local private = {
@@ -109,8 +112,9 @@ function TableOfWorkTime ()
     return public
 end
 
-tableOfWorkTime = TableOfWorkTime()
+local tableOfWorkTime = TableOfWorkTime()
 F.time = tableOfWorkTime.print
+
 
 F.exit = function()
     F.stop()
@@ -119,13 +123,14 @@ F.exit = function()
 end
 
 
-printToConsoleAndInFile = function (string)
+function printToConsoleAndInFile (string)
     local outString = os.date("%X") .. ": " .. string .. ".\n"
     io.write(outString) 
     File("work_log.md").write(outString)
 end
 
-function Timer ()
+
+function Timer (tableOfWorkTime)
     local private = {}
     local public  = {}
     public.start = function (ticketName)
@@ -148,7 +153,7 @@ function Timer ()
     return public
 end
 
-local timer = Timer()
+local timer = Timer(tableOfWorkTime)
 F.start = timer.start
 F.stop  = timer.stop
 
