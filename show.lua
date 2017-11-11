4d5bf319cea1793d53e427f58d4fe9d12eced3c7
dmitriy.pavlyuk <al.lastor@bk.ru>
2017-11-11 19:47:34 +0300

    Добавлен показ таблицы за текущий месяц

--- show.lua
+++ show.lua
@@ -2,27 +2,48 @@ function Show (index)
     local private = {}
     local public  = {}
 
-    private.sumTable = function () 
+    private.isInCurrentMonth = function (date)
+        local currentMonth = os.date("%Y.%m.")
+        return date:match(currentMonth .. "(.*)")
+    end
+
+    private.printTable = function (table)
+        io.write(
+            line ..
+            table.tableBody() .. 
+            line ..
+            table.tableBottom{sumString = "ok"} ..
+            line
+        )
+    end
+
+    private.generalTablePrint = function (filter)
         local sumTable, tmpTable = TableOfWorkTime(), TableOfWorkTime()
         for _, tableName in pairs(index.getIndex()) do   
+            if filter and (not private.isInCurrentMonth(tableName)) then
+                break
+            end
+
             tmpTable.load("tables/".. tableName ..".lua")
             for key, val in pairs(tmpTable.table()) do
                 sumTable.addIn(key, val)
         end end
 
-        io.write(
-            line ..
-            sumTable.tableBody() .. 
-            line ..
-            sumTable.tableBottom{sumString = "ok"} ..
-            line
-        )
+        private.printTable(sumTable)
     end
+
+    private.sumTable     = function () private.generalTablePrint()     end
+    private.currentMonth = function () private.generalTablePrint("ok") end
 
     public.table = function (tableName) 
         if tableName == "sum table" then
             private.sumTable()
             return    
+        end
+
+        if tableName == "current month" then
+            private.currentMonth()
+            return
         end
 
         local fileName = "tables/".. tableName ..".lua"
